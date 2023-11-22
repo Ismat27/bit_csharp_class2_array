7c08bdfdd691542dd25a06a51e3c4f3f69c3ddca
ismat27 <adegbiteismail27@gmail.com>
2023-11-22 20:47:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿var studentNames = new string[] {
+    "Mobola Makay", "Tomi Olaoye", "Suleiman Ozigis", "Uchenna Doe", "Temmy Orekunrin",
+    "Ismail Adegbite", "Victor Emetole", "Kayode Emmanuel", "Tolu Obasun", "Uche Ijomanta"
+};
+
+List<int> studentAges = new List<int>()
+{
+    17, 19, 18, 20, 16, 21, 18, 22, 17, 19
+};
+
+Random random = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+    studentAges.Add(random.Next(18, 27));
+}
+
+string isStudent18Present = studentAges.Contains(18)? "Yes" : "No";
+Console.WriteLine($"Is there a student whose age is 18? {isStudent18Present}");
+
+Console.WriteLine("Student Ages");
+foreach (var studentAge in studentAges) 
+    Console.WriteLine(studentAge);
+
+Console.WriteLine("\nStudent Names");
+foreach (var studentName in studentNames)
+{
+    Console.WriteLine(studentName);
+}
